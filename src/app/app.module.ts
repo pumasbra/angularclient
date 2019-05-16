b2689a706aa56c77abf7445f19e1512be50864ae
Brahian Velazquez Tellez <brahian_velazquez@hacienda.gob.mx>
2019-05-16 13:34:28 -0500

    se agregaron los components de user-form y user-list, asi como el servicio user-service y el arvhivo app-routing module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { UserListComponent } from './user-list/user-list.component';
+import { UserFormComponent } from './user-form/user-form.component';
+import { AppRoutingModule } from './app-routing.module';
+import { UserService } from './user-service.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserListComponent,
+    UserFormComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [UserService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
